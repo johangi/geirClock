2e1096eda10959d8d6df0c6c6509b77b8e542882
johangi <johangiaever@gmail.com>
2022-10-14 15:19:52 +0200

    geir hands fixed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ setInterval(function() {
           date.getMinutes() / 60,
           date.getSeconds() / 60
         ],
-        clock = document.querySelector('.clock');
+        clock = document.getElementById('clock1');
   
     positions.forEach(function(rotation, index) {
       clock.children[index].style.transform = 'rotate(' + rotation + 'turn)';
@@ -22,17 +22,54 @@ setInterval(function() {
     positions(function(rotation, index) {
       img[index].style.transform = 'rotate(' + rotation + 'turn)';
     });
+
+  
   
   }, 1000);
-    
+
+  setInterval(function() {
+    var date = new Date(),
+        positions = [
+          date.getHours() / 12,
+          date.getMinutes() / 60,
+          date.getSeconds() / 60
+        ],
+        clock = document.getElementById('clock2');
+  
+    positions.forEach(function(rotation, index) {
+      clock.children[index].style.transform = 'rotate(' + rotation + 'turn)';
+    });
+  
+  }, 1000);
+
+  setInterval(function() {
+    var date = new Date(),
+        positions = (date.getSeconds() / 60)
+        ,
+        img = document.querySelector('#geirIMG');
+  
+    positions(function(rotation, index) {
+      img[index].style.transform = 'rotate(' + rotation + 'turn)';
+    });
+
+  
+  
+  }, 1000);
+  
     let geir = true
-    const geirIMG = document.querySelector(".geirIMG");
+    let geirIMG = document.querySelector(".geirIMG");
+    let clockDiv1 = document.getElementById("clock1")
+    let clockDiv2 = document.getElementById("clock2")
     document.querySelector('body').addEventListener('click', e => {
       if(geir === false){
         geirIMG.setAttribute("src", "./img/gairer.png")
+        clockDiv1.classList.remove('hidden')
+        clockDiv2.classList.add('hidden')
         geir = true
       } else if(geir === true){
         geirIMG.setAttribute("src", "./img/geir.jfif")
+        clockDiv1.classList.add('hidden')
+        clockDiv2.classList.remove('hidden')
         geir = false
       }
     })
